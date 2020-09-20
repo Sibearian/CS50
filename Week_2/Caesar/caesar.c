8259c5322504ceab706adc8bc071870e31bed35c
Srujan <50288128+Sibearian@users.noreply.github.com>
2020-09-20 17:16:32 +0530

    Update caesar.c

--- Week_2/Caesar/caesar.c
+++ Week_2/Caesar/caesar.c
@@ -7,6 +7,23 @@
 void encrypt(char letter, int key);
 int check_key_string(string k);
 
+/*
+------------------------ main ------------------------
+Main function of the program. It
+
+-This function encrypts a plain text to cyphertext using caesar encryption
+
+params:zzz
+    (int, string)
+    argc - to count the number of command line arguments
+    argv - the arguments from the command line
+
+returns: int
+    - returns 1, if the number of command arguments are not equal to 2
+                 or the second argument is a not a digit
+    - returns 0, if it worked
+------------------------------------------------------
+*/
 
 int main(int argc, string argv[])
 {
@@ -23,6 +40,8 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    key = atoi(argv[1]);
+
     string plain_text = get_string("plaintext :  ");
 
     printf("ciphertext : ");
@@ -34,8 +53,11 @@ int main(int argc, string argv[])
 
     printf("\n");
 
-
 }
+
+/*
+--------------------------------------------------------------------------
+*/
 
 
 /*
@@ -45,13 +67,16 @@ A function to encrypt alphabet
 -This function will take a single character a time.
 -It will encrypt every ascii alpahbet (A - Z and a - z)
 -It will NOT encrypt anything else
+-Prints the encrypted alphabets to the output
 
 Params:
     (char, key)
     char - a ascii char should be passed
     key  - a integer should be passed
 
--------------------------------------------------*/
+returns: Void
+-------------------------------------------------
+*/
 
 void encrypt(char letter, int key)
 {
@@ -76,7 +101,7 @@ void encrypt(char letter, int key)
 
 
 /*
----------- check_key_string ----------
+-------------- check_key_string --------------
 A function to check if the key has any problem
 
 -It checks if the key has a ascii character that is NOT a digit
@@ -84,7 +109,12 @@ A function to check if the key has any problem
 params:
     (string)
     k - a string of potential number
---------------------------------------*/
+
+returns: int
+    - returns 1, if the string does have anything other than a digit
+    - returns 0, if everything is ok
+----------------------------------------------
+*/
 
 int check_key_string(string k)
 {
@@ -92,10 +122,10 @@ int check_key_string(string k)
     {
         if (!(isdigit(k[i])))
         {
-            return -1;
+            return 1;
         }
     }
 
-    return (int) atoi(k);
+    return 0;
 
 }
